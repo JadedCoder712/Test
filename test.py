49e9baab08d31f6dd15bb2548aa941a0655f8c53
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-01-26 17:47:30 -0500

    Updated from Brython Server: 1/26/2017 5:47:29 PM

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 user = input("What is your name?")
 user2 = input("What is five plus six, {0}?".format(user))
-if user2 is 11:
+if int(user2) is 11:
     print("That's correct! Five plus six is {0}. And that minus two is {1}.".format(user2, int(user2)-2))
-print("You're very smart, {0}!".format(user))
+    print("You're very smart, {0}!".format(user))
+if int(user2) > 11:
+    user3 = input("No, {0} is too high! Try again, {1}. What is five plus six?".format(user2, user))
+    if int(user3) is not 11:
+        print("You're hopeless!")
+if int(user2) < 11:
+    user4 = input("No, {0} is too low! Try again, {1}. What is five plus six?".format(user2, user))
+    if int(user4) is not 11:
+        print("You're hopeless!")
