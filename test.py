e6b8ba772663ad1c2d74598297163717e4ff6250
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-01-26 17:12:13 -0500

    Updated from Brython Server: 1/26/2017 5:12:12 PM

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
-# test by kyle
-# you are dreaming
+user = input("What is your name?")
+user2 = input("What is five plus six, {0}?".format(user))
+print("That's correct! Five plus six is {0}. And that minus two is {1}.".format(user2, int(user2)-2))
