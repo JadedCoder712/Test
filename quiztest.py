37740a22b87b62bbde89032f8bbcc095e6ecdef8
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-01-27 13:04:50 -0500

    Updated from Brython Server: 1/27/2017 1:04:49 PM

--- quiztest.py
+++ quiztest.py
@@ -1,1 +1,12 @@
-
+user = input("Would 3 = val be considered valid in Python? 1 for yes, 0 for no.")
+if int(user) is not 0:
+    user = input("You are wrong.")
+user2 = input("Suppose you type a = 5 then b = a. Is this valid in Python? 1=yes, 0=no")
+if int(user2) is not 1:
+    user2 = input("You are wrong!")
+user3 = input("What is the final value of b if you type a = 5 then b = a? Type your response as a number.")
+if int(user3) is not 5:
+    user3 = input("Wrong!")
+user4 = input ("What is the final value of b if you type a = 5 then b = a? Type your response as a number.")
+if int(user4) is not 5:
+    user4 = input("You are wrong!")
