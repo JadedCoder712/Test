5691fde25b100a20b14e230abd02f131f0e301cd
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-01-27 12:51:02 -0500

    Updated from Brython Server: 1/27/2017 12:51:01 PM

--- test1.py
+++ test1.py
@@ -1,1 +1,13 @@
-
+user = input("What is your name?")
+temp = input("What is the temperature outside today, {0}?".format(user))
+user2 = input("Are you going outside today, {0}? Enter 1 for yes, 0 for no.".format(user))
+if user2 is str(0):
+    if temp < 69:
+        print("That makes sense, as the temperature is {0}. Have a nice day, {1}.".format(temp, user))
+    if temp > 70:
+        print("You really should, {0}! The temperature is a warm {1}.".format(user, temp))
+if user2 is str(1):
+    if int(temp) < 69:
+        print("Wear pants, {0}.".format(user))
+    if int(temp) > 70:
+        print("Wear shorts, {0}! And be grateful you're not a computer!".format(user))
